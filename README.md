46ef9ce971c747828544ee5e26ae622fff386f81
Bruno-VILLALON <brunovillalon@gmail.com>
2022-09-21 17:55:26 +0200

    add comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculette
 
+Projet permettant de faire des calculs simples, réalisé avec Html, Css et JavScript.
+
+La calculette réagit au clic et aux touches du pavé numérique.
+
+l'affichage s'adapte à la taille de l'écran de la calculette.
--- app.js
+++ app.js
@@ -1,36 +1,53 @@
+// Je récupère les éléments du DOM
 const keys = [...document.querySelectorAll('.key')];
 const keycodeList = keys.map(key => key.dataset.key);
 const screen = document.querySelector('.screen');
 
+
+//J'écoute les événements à la pression des touches du clavier
 document.addEventListener('keydown', (event) =>{
     const value = `${event.key}`;
     //console.log(value, typeof value);
     calc(value);
 })
 
+//J'écoute les événements au clic
 document.addEventListener('click', (event) =>{
     const value = event.target.dataset.key;
-    //console.log(value);
+    //console.log(value, typeof value);
     calc(value);
 })
 
+
+//FONCTION pour Calculer
 const calc = (value) => {
+    //SI "value" est bien inclus dans "keycodeList"
     if (keycodeList.includes(value)) {
+        //ALORS
         switch (value) {
+            //dans le cas où "value = Backspace"
             case 'Backspace':
+                //j'affiche une chaîne de caractère VIDE
                 screen.textContent = "";
                 break;
+            //dans le cas où "value = Enter"
             case 'Enter':
+                //je récupère ce qu'il y a dans "screen"
                 const calcul = eval(screen.textContent);
+                //et j'affiche le résultat dans "screen"
                 screen.textContent = calcul;
                 break;
+            //dans tous les autres cas
             default:
+                //je récupère l'index de la touche
                 const keycodeIndex = keycodeList.indexOf(value);
+                //je défini la valeur de la touche en fonction de l'index
                 const key = keys[keycodeIndex];
+                //j'affiche dans "screen" la valeur de la touche
                 screen.textContent += key.innerHTML;
         }
     }
-
+    //Je modifie la taille de la police en fonction du nombre de caractère
     if (screen.textContent.length <= 6) {
         screen.style.fontSize = '4rem'
     }
@@ -45,8 +62,7 @@ const calc = (value) => {
     }
 }
 
+//J'affiche les erreurs dans "screen"
 window.addEventListener('error', (event) => {
     screen.textContent = event.message
 })
-
-
